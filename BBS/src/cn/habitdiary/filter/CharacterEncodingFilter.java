874f2e85ecb1ab5695928f493505bf8e6113bec5
Junyu Xie <573668878@qq.com>
2018-02-20 00:02:57 +0800

    Add files via upload

---
+++ BBS/src/cn/habitdiary/filter/CharacterEncodingFilter.java
@@ -0,0 +1,28 @@
+package cn.habitdiary.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+public class CharacterEncodingFilter implements Filter {
+    private FilterConfig config;
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        req.setCharacterEncoding(config.getInitParameter("charset"));
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+        this.config = config;
+    }
+
+    public FilterConfig getConfig() {
+        return config;
+    }
+
+    public void setConfig(FilterConfig config) {
+        this.config = config;
+    }
+
+
+}
